72ad7a5d9db88286d5193f712f571e982f198b94
liuchong <liuchong@viomi.com.cn>
2019-06-27 19:16:25 +0800

    init

---
+++ vue-cli-demo/src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router/index'
+import VueResource from 'vue-resource'
+
+
+Vue.config.productionTip = false;
+
+Vue.use(router);
+Vue.use(VueResource)
+
+new Vue({
+    router,
+  render: h => h(App),
+}).$mount('#app');
